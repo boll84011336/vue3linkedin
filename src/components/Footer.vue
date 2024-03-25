5a78694cdc170facd36f6295d52041eb5c24ae65
boll84011336 <boll84011336@gmail.com>
2024-03-26 01:28:46 +0800

    footer 修改

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,28 +1,21 @@
 <template>
   <footer class="d-flex">
     <!-- Footer content goes here -->
-    <div class="logo mt-4 ms-4">
-      <img alt="logo" src="../assets/logo.png" />
+    <div class="logo">
+      <img alt="logo" src="@/assets/logo.png" />
     </div>
-    <nav d-flex flex-column>
+    <nav class="d-flex">
       <!-- 上下兩塊 -->
-      <div class="nav_title d-flex mb-5">
+      <div class="nav_title_block">
         <h6>Navigation</h6>
-        <h6></h6>
-        <h6></h6>
-        <h6>Fast access</h6>
-        <h6>Language</h6>
-      </div>
-
-      <div>
-        <div class="Navigation d-flex">
+        <div class="nav_title_content d-flex">
           <div class="nav_body">
             <p>Home</p>
             <p>About</p>
             <p>Services</p>
             <p>Portfolio</p>
           </div>
-          <div class="nav_body">
+          <div class="nav_body middle">
             <p>Talent Solutions</p>
             <p>Marketing Solutions</p>
             <p>Sales Solutions</p>
@@ -33,6 +26,44 @@
             <p>Privacy & Terms </p>
             <p>Mobile App</p>
           </div>
+        </div>
+        <!-- <h6></h6>
+        <h6></h6>
+        <h6 class="fast_access_title">Fast access</h6>
+        <h6 class="language_title">Language</h6> -->
+      </div>
+
+      <div class="block2">
+        <h6 class="fast_access_title">Fast access</h6>
+        <div class="fast_access_body one d-flex">
+          <div class="questions">
+            <a class="">Questions?</a>
+            <img src="../assets/help.png" alt="Questions">
+          </div>
+        </div>
+        <div class="fast_access_body mt-3">
+          <div class="settings">
+            <a class="">Settings?</a>
+            <img src="../assets/settings.png" alt="Settings">
+          </div>
+        </div>
+      </div>
+
+      <div class="block3">
+        <h6 class="language_title">Language</h6>
+        <div class="language_options">
+          <select name="" id="">
+            <option value="en">English</option>
+            <option value="zh">中文</option>
+            <option value="jp">日本語</option>
+          </select>
+        </div>
+      </div>
+
+      
+
+      <!-- <div>
+        <div class="Navigation d-flex">
 
           <div class="fast_access">
             <div class="nav_body">
@@ -61,7 +92,7 @@
         </div>
         
         
-      </div>
+      </div> -->
     </nav>
   </footer>
 </template>
@@ -74,74 +105,101 @@ export default {
 };
 </script>
 
-<style scoped>
-footer {
-  margin-left: 130px;
-  margin-right: 130px;
-}
-
-.nav_title h6 {
-  width: 160px;
-  font-weight: 800;
-}
-
-.nav_title h6:nth-child(2) {
-  background: firebrick;
-}
-
-.nav_title h6:nth-child(3) {
-  background: sandybrown;
-}
-
-.nav_title h6:nth-child(5) {
-  margin-left: 30px;
-} 
-
-.Navigation .nav_body {
-  width: 160px;
-}
-
-.Navigation .nav_body:nth-child(2) {
-
-}
-
-
-.fast_access {
-  margin-left: 50px;
-}
-
-.language {
+<style lang="scss" scoped>
+  footer {
+    background-color: #fff;
+    height: 215px;
+    padding: 30px 130px;
+  }
   
-}
+  .logo {
+    margin-right: 69px;
+  }
 
-.questions, .settings {
-  border: 1px solid #000;
-  padding: 5px 10px;
-  border-radius: 5px;
-}
+  nav {
+    text-align: left;
+    padding: 0px
+  }
 
-.fast_access .nav_body {
-  background-color:  #fff;
-  color: #0275B1;
-  width: 170px;
-}
+  .nav_title_block {
+    width: 485px;
+    h6 {
+      font-weight: 700;
+      margin-bottom: 30px;
+    }
+  }
 
-.fast_access .nav_body a {
-  text-decoration: none;
-}
+  .nav_body {
+    width: 135px;
+    font-size: 13px;
+    p {
+      margin-bottom: 10px;
+    }
+  }
 
-/* 滑過時的樣式 */ /* 點擊時的樣式 */
-.fast_access .nav_body:hover,
-.fast_access .nav_body:active {
-  background-color: #0275B1; 
-  color: #fff; 
-}
+  .middle {
+    margin-left: 35px;
+    margin-right: 45px;
+  }
 
-.language select {
-  width: 270px;
-  height: 36px;
-  border-radius: 5px;
-  margin-left: 40px;
-}
+  .block2 {
+    width: 170px;
+    margin-left: 65px;
+    margin-right: 60px;
+  }
+  
+  .fast_access_title, .language_title {
+    margin-bottom: 23px;
+    font-weight: 700;
+  }
+
+  .fast_access_body {
+    width: 170px;
+    height: 32px;
+    border: 1px solid #0275B1;
+    border-radius: 4px;
+    .questions {
+      a {
+        text-decoration: none;
+        padding: 11px 43px 10px 15px;
+      }
+      img {
+        width: 16px;
+      }
+    }
+    .settings {
+      a {
+        text-decoration: none;
+        padding: 11px 58px 10px 15px;
+        color:#0275B1;
+      }
+      img {
+        width: 16px;
+      }
+    }
+   
+  }
+
+  .one {
+    background-color: #0275B1;
+    a {
+      color:#fff;
+    }
+  } 
+ 
+  .block3 {
+    width: 270px;
+  }
+
+  .language_options {
+    select {
+      width: 270px;
+      height: 32px;
+      border: 1px solid #F0F0F0;
+    }
+  }
+  
+
+
 /* Component styles go here */
 </style>
