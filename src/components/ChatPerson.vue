ed7ff7ee30b220edfc5c797c23a6e8e618e92a7f
boll84011336 <boll84011336@gmail.com>
2024-04-01 20:19:50 +0800

    add chat page

---
+++ src/components/ChatPerson.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="d-flex info first">
+		<div class="photo"><img :src="person.img" :alt="person.name"></div>
+		<div class="script">
+				<p class="f-14">{{ person.name }}</p>
+				<p class="f-12">{{ person.message }}</p>
+		</div>
+	</div>
+</template>
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    props: {
+      person: Object, //接收父層來的資料
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+		.info {
+			padding-left: 30px;
+			height:87px;
+			.photo {
+				width: 52px;
+				margin-right: 15px;
+			} 
+			.script {
+				p {
+					margin: 0;
+				}
+				.f-14 {
+					font-size: 14px;
+					font-weight: 700;
+				}
+				.f-12 {
+					font-size: 12px;
+					color: grey;
+				}
+			}
+		}
+		.first {
+			height: 87px;
+			padding-top: 10px;
+		}
+ 
+  </style>
+  
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,19 +1,130 @@
-<template>
-  <div>
-    <!-- -->
-  </div>
-</template>
-
 <script>
 import { ref } from 'vue';
+import ChatPerson from '@/components/ChatPerson.vue';
 
 export default {
+  components: { ChatPerson },
   setup() {
-    const feature = ref();
+    const personMessageList = ref([
+      {	img: require('@/assets/chats/1.png'), name: "Darlene Black", message: "Hey, how is your project?"},
+      {	img: require('@/assets/chats/2.png'), name: "Theresa Steward", message: "Hi, Dmitry! I have a work for you. We"},
+      {	img: require('@/assets/chats/3.png'), name: "Brandon Wilson", message: "I am Russian and I am learning Engl"},
+      {	img: require('@/assets/chats/4.png'), name: "Kyle Fisher", message: "So, It’s up to you!"},
+      {	img: require('@/assets/chats/5.png'), name: "Audrey Alexander", message: "When you got it?"},
+      {	img: require('@/assets/chats/6.png'), name: "Design Conference", message: "Can you guys help me with it?"}
+    ]);
 
     return {
-      
+      personMessageList
     };
   },
 };
 </script>
+
+<template>
+  <div class="chats d-flex">
+    <div class="left">
+      <!-- -->
+      <h5>Chats</h5>
+    
+      <chat-person v-for="item in personMessageList" :key="item.name" :person="item"/>
+    </div>
+
+    <div class="right">
+      <div class="block1 d-flex justify-content-between">
+        <div><h6>Chat with<span class="txt-blue"> Kyle Fisher</span><span class="gray">last online: 4 hours ago</span></h6></div>
+        <div class="share">Shared media (12)</div>
+      </div>
+      <div class="message_box">
+        <div class="message_left mt-2 pt-4">
+          <img src="@/assets/chats/ms1.png" alt="">
+        </div>
+        <div class="message_right mt-2">
+          <img src="@/assets/chats/ms2.png" alt="">
+        </div>
+        <div class="message_left mt-2">
+          <img src="@/assets/chats/ms3.png" alt="">
+        </div>
+        <div class="message_right mt-2">
+          <img src="@/assets/chats/ms4.png" alt="">
+        </div>
+      </div>
+
+      <div class="input_message d-flex justify-content-between mt-5">
+        <div>
+          <input type="text" placeholder="Write your message">
+        </div>
+        <div class="send_btn">
+          <a href="#"><img src="@/assets/sendbtn.png" alt="send"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>  
+  .chats {
+    background-color: rgb(245, 247, 250);
+    padding: 40px;
+    padding-left: 130px;
+    width: 1440px;
+    .left {
+      width: 290px;
+      height: 560px;
+      background-color: #fff;
+      text-align: left;
+      h5 {
+        padding-top: 25px;
+        padding-left: 30px;
+        font-size: 12px;
+        font-weight: 600;
+        font-family: Gotham Pro;
+      }
+    }
+    .right {
+      margin-left: 40px;
+      width: 850px;
+      background-color: #fff;
+      padding: 25px 30px 20px 28px;
+      h6,.share {
+        font-weight: 600; 
+      }
+      .txt-blue {
+        color: #0275B1;
+      }
+      .gray {
+        font-weight: normal;
+        margin-left: 15px;
+        color:rgb(219, 219, 219)
+      }
+      .input_message {
+        input {
+          width: 750px;
+          height: 53px;
+          border: none;
+          border-top:1px solid rgb(219, 219, 219);
+        }
+        input:focus-visible {
+					outline: none;
+				}
+        .send_btn {
+          display: flex;
+          align-items: center;
+          
+        }
+      }
+
+      .message_box {
+        .message_left {
+          text-align: left;
+        }
+        .message_right {
+          text-align: right;
+        }
+      }
+     
+    }
+  }
+</style>
